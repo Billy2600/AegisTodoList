9aacce5435f60b869434f08d60412256addb3ebf
Billy McPherson <billymcfly@gmail.com>
2021-02-24 11:26:56 -0700

    Add new list item submission form and functionality

--- AegisTodoList/ClientApp/src/app/home/home.component.ts
+++ AegisTodoList/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, NgModule } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { TodoListItem } from './todoListItem';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +10,36 @@ import { TodoListItem } from './todoListItem';
 })
 export class HomeComponent {
   public todoListItems: TodoListItem[];
+  private readonly controllerPath = 'TodoList';
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<TodoListItem[]>(baseUrl + 'TodoList').subscribe(result => {
+  newListItemForm = this.formBuilder.group({
+    description: '',
+    completed: false
+  })
+
+  constructor(
+    private http: HttpClient, 
+    @Inject('BASE_URL') private baseUrl: string,
+    private formBuilder: FormBuilder
+  ) {
+    this.loadListItems();
+  }
+
+  loadListItems(): void {
+    this.http.get<TodoListItem[]>(this.baseUrl + this.controllerPath).subscribe(result => {
       this.todoListItems = result;
     }, error => console.error(error));
+  }
+
+  addListItem(): void {
+    let listItem : TodoListItem = {
+      todoListItemID: 0,
+      description: this.newListItemForm.get('description').value,
+      completed: this.newListItemForm.get('completed').value
+    };
+
+    this.http.post<TodoListItem>(this.baseUrl + this.controllerPath, listItem).subscribe(result =>
+      this.loadListItems()
+    );
   }
 }
